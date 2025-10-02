7f4e39f9b12c0546b0f09033202c5ea3b49e2702
Rodion Clepa <rclepa>
2025-10-02 15:57:45 +0300

    feat: add header top bar and change layout logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,40 @@
 import "./App.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import Uikit from "./pages/Uikit";
 import { Products } from "@/pages/Products";
+import { Header } from "@/layout/Header";
+
+function Layout() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
+}
 
 const router = createBrowserRouter([
   {
-    path: "/uikit",
-    element: <Uikit />,
-  },
-  {
-    path: "/products",
-    element: <Products />,
+    element: <Layout />,
+    children: [
+      {
+        path: "/uikit",
+        element: <Uikit />,
+      },
+      {
+        path: "/products",
+        element: <Products />,
+      },
+    ],
   },
 ]);
 
 function App() {
-  return <RouterProvider router={router}></RouterProvider>;
+  return (
+    <>
+      <RouterProvider router={router}></RouterProvider>
+    </>
+  );
 }
 
 export default App;
---
+++ src/constants/pageActions.ts
@@ -0,0 +1,11 @@
+export const languageOptions = [
+  { value: "en", label: "English" },
+  { value: "es", label: "Español" },
+  { value: "fr", label: "Français" },
+  { value: "de", label: "Deutsch" },
+];
+
+export const currencyOptions = [
+  { value: "usd", label: "USD" },
+  { value: "mdl", label: "MDL" },
+];
